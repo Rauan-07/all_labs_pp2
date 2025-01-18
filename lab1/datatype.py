abc590b7e35a6385e53e3c79c00dcc3da5b72509
Rauan <r_yerkhan@kbtu.kz>
2025-01-18 22:27:33 +0500

    lab 1

---
+++ lab1/datatype.py
@@ -0,0 +1,9 @@
+x = 5
+print(type(x))
+x = "Hello World"	#str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range
---
+++ lab1/string.py
@@ -0,0 +1,25 @@
+print("Hello")
+print('Hello')
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#slicing strings
+b = "Hello, World!"
+print(b[2:5])
+
+#modify srtings
+a = "Hello, World!"
+print(a.upper())
+a = "Hello, World!"
+print(a.lower())
+
+#concatenate strings
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#format strings
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
